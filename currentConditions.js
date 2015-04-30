a60a78e50cd0a5f178a0bd603713362b9182e84a
Doug Wallace <dwallace@linkedin.com>
2015-04-30 12:39:15 -0700

    Added the ability to display more than one locale.

--- currentConditions.js
+++ currentConditions.js
@@ -1,13 +1,26 @@
 $(function() {
-  $.getJSON( "data.php?current_conditions", function( data ) {
-    if(data.status) {
-      console.error(data.status);
-    }else {
-      console.log(data);
-      $('.current-cond-city').html(data.current_observation.display_location.full);
-      $('.current-cond-temp').html(data.current_observation.feelslike_f+'&deg; F');
-      $('.current-cond-text').html(data.current_observation.weather);
-      $('.current-cond-icon').attr('src', data.current_observation.icon_url);
-    }
-  });
+
+  var locale = $('.forecast-locale');
+
+  for(var i=0; i<locale.length; i++) {
+    (function(i){
+      var $locale = $(locale[i]),
+          city = $locale.attr('data-city'),
+          state = $locale.attr('data-state');
+      $.getJSON( 'data.php?current_conditions&city='+city+'&state='+state, function( data ) {
+        if(data.status) {
+          console.error(data.status);
+        }else {
+          console.log(data);
+          $($locale.find('.current-cond-city')[0]).html(data.current_observation.display_location.full);
+          $($locale.find('.current-cond-temp')[0]).html(data.current_observation.feelslike_f+'&deg; F');
+          $($locale.find('.current-cond-text')[0]).html(data.current_observation.weather);
+          $($locale.find('.current-cond-icon')[0]).attr('src', data.current_observation.icon_url);
+          $($locale.find('.current-cond-wind')[0]).html(data.current_observation.wind_string);
+        }
+      });
+    })(i);
+  }
+
+
 });
--- extendedForecast.js
+++ extendedForecast.js
@@ -1,23 +1,34 @@
 $(function() {
-  var days,
-      template;
-      $container = $('.extended-forecast-cont');
 
-  $.getJSON( "data.php?10_day", function( data ) {
-    if(data.status) {
-      console.error(data.status);
-    }else {
-      console.log(data.forecast.simpleforecast.forecastday);
-      days = data.forecast.simpleforecast.forecastday;
-      for(var i=0; i<5; i++) {
-        template = '<div class="two columns day">';
-        template += '<h5 class="extended-forecast-date">'+days[i].date.weekday_short+" "+days[i].date.month+'/'+days[i].date.day+'</h5>';
-        template += '<img class="extended-forecast-icon" src="'+days[i].icon_url+'"/>';
-        template += '<h5 class="extended-forecast-high"><span>Hi: </span>'+days[i].high.fahrenheit+'&deg;</h5>';
-        template += '<h5 class="extended-forecast-low"><span>Lo: </span>'+days[i].low.fahrenheit+'&deg;</h5>';
-        template += '</div>'
-        $container.append(template);
-      }
-    }
-  });
+  var locale = $('.forecast-locale');
+
+  for(var i=0; i<locale.length; i++) {
+    (function(i){
+      var $locale = $(locale[i]),
+          city = $locale.attr('data-city'),
+          state = $locale.attr('data-state'),
+          days,
+          template,
+          container = $locale.find('.extended-forecast-cont')[0];
+      console.log(container);
+      $.getJSON( 'data.php?10_day&city='+city+'&state='+state, function( data ) {
+        if(data.status) {
+          console.error(data.status);
+        }else {
+          console.log(data.forecast.simpleforecast.forecastday);
+          days = data.forecast.simpleforecast.forecastday;
+          for(var i=0; i<5; i++) {
+            template = '<div class="two columns day">';
+            template += '<h5 class="extended-forecast-date">'+days[i].date.weekday_short+" "+days[i].date.month+'/'+days[i].date.day+'</h5>';
+            template += '<img class="extended-forecast-icon" src="'+days[i].icon_url+'"/>';
+            template += '<h5 class="extended-forecast-high"><span>Hi: </span>'+days[i].high.fahrenheit+'&deg;</h5>';
+            template += '<h5 class="extended-forecast-low"><span>Lo: </span>'+days[i].low.fahrenheit+'&deg;</h5>';
+            template += '<p class="wind"><span class="wind-dir">'+days[i].avewind.dir+' </span><span class="wind-speed">'+days[i].avewind.mph+'mph</span></p>';
+            template += '</div>'
+            $(container).append(template);
+          }
+        }
+      });
+    })(i);
+  }
 });
